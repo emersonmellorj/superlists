ad6df9f231163d7483db09a55f0bb0373dda667f
Emerson Mello <emersonmello@iMac-de-Emerson.local>
2019-11-14 10:08:10 -0300

    First unit test and url mapping, dummy view.

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,25 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
     
-    def test_bad_maths(self):
-        self.assertEqual(1+1,3,'Falha na soma dos valores!')
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        
+        # Testando se o nome da view chamada pela URL / e a home_page
+        self.assertEqual(found.func, home_page)
+        
+    
+    def test_home_page_returns_correct_html(self):
+        
+        request = HttpRequest()
+        
+        # Response = uma view criado recebendo um request como parametro
+        response = home_page(request)
+        html = response.content.decode('UTF-8')
+        self.assertTrue(html.startswith('<htm>))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
---
+++ lists/urls.py
@@ -0,0 +1,9 @@
+# URLs da aplicacao lists
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home_page, name='home')
+]
