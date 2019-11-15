880daa512cee7eb4d991dde43828119c36aab5e6
Emerson Mello <emersonmello@iMac-de-Emerson.local>
2019-11-15 19:16:00 -0300

    Criacao do metodo check_for_row_in_list_table

--- superlists/scripts/functional_tests.py
+++ superlists/scripts/functional_tests.py
@@ -14,6 +14,13 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+    
+    def check_for_row_in_list_table(self, row_text):
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+        
         
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith ouviu falar de uma nova aplicacao online interessante para lista de tarefas. Ela decide verificar sua HP.
@@ -37,15 +44,13 @@ class NewVisitorTest(unittest.TestCase):
         # Quando ela tecla enter, a pagina e atualizada, e agora a pagina lista "1: Buy peacock feathers" com um item em uma lista de tarefas
         inputbox.send_keys(Keys.ENTER)
         time.sleep(3)
-        
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        self.check_for_row_in_list_table('1 - Buy peacock feathers')
         #self.assertTrue(any(row.text == '1 - Buy peacock feathers' for row in rows), f"New to-do item not appear in table. Contents were: \n{table.text}")
-        self.assertIn('1 - Buy peacock feathers', [row.text for row in rows])
         
         # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item. Ela insere "Use peacock feathers to make a fly". Edith e bem metodica.
         inputbox = self.browser.find_element_by_id('id_new_item')
         time.sleep(3)
+        
         inputbox.send_keys('2 - Use peacock feathers to make a fly')
         time.sleep(3)
         
@@ -53,12 +58,9 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(3)
         
         # A pagina e atualizada novamente e agora mostra os dois itens em sua lista.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        self.check_for_row_in_list_table('1 - Buy peacock feathers')
+        self.check_for_row_in_list_table('2 - Use peacock feathers to make a fly')
         
-        self.assertIn('1 - Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2 - Use peacock feathers to make a fly', [row.text for row in rows])
-
         # Edith se pergunta se o site lembrara da sua lista. Entao ela nota que o site gerou uma URL unica pra ela -- ha um pequeno texto explicativo para isso.
         self.fail('Finish the test!')
 
