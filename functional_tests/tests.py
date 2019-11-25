f922c6fbeff82ea163aff6c4a7d428338b404cae
Emerson Mello <emersonmello@iMac-de-Emerson.local>
2019-11-25 16:06:29 -0300

    first steps on FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -38,6 +38,25 @@ class NewVisitorTest(LiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
+    
+    
+    def test_layout_and_styling(self):
+        
+        # Edith acessa a pagina inicial
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        
+        # Ela percebe que a caixa de entrada esta elegantemente centralizada
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
+        
+        # Ela inicia uma nova lista e ve que a entrada esta elegantemente centralizada ai tambem
+        inputbox.send_keys('1 - testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1 - testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
+        
             
         
     def test_can_start_a_list_for_one_user(self):
