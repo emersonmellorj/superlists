7e325bb8608a72f6d1ac6beb53ac7b96b836d6c0
Emerson Mello <emersonmello@iMac-de-Emerson.local>
2019-11-25 12:12:09 -0300

    Separando as URL em arquivos de projeto e aplicacao.

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from lists import views as list_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('lists.urls')),
+    path('java_script/', list_views.java_script, name='java_script'),
+    re_path(r'^$', list_views.home_page, name='home'),
+    re_path(r'^lists/', include('lists.urls')),
 ]
